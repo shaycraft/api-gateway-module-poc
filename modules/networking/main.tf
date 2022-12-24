ed6ebab8926adae373372a78411be5a492e51bd0
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-24 01:07:58 -0700

    Adds public subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,7 +1,15 @@
 data "aws_availability_zones" "available" {}
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "${var.namespace}-vpc"
-  cidr   = "10.0.0.0/16"
+  source                       = "terraform-aws-modules/vpc/aws"
+  name                         = "${var.namespace}-vpc"
+  cidr                         = "10.0.0.0/16"
+  public_subnets               = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs                          = data.aws_availability_zones.available.names
+  create_database_subnet_group = false
+  enable_nat_gateway           = false
+  tags = {
+    Name      = "vpc terraform module poc"
+    CreatedBy = "terraform"
+  }
 }
